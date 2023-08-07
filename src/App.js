a4e1418c53f1e416496bc5da90e1baff362601a6
MonjedNidal <monjed1010@gmail.com>
2023-08-07 14:54:32 +0300

    first-commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./css/App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
